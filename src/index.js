3b7ceee547b208501b6f5e0173f77e00f8931b41
Akhil Bisht <akhilbisht798@gmail.com>
2022-10-10 23:46:00 +0530

    Clean up

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
-//http://api.openweathermap.org/data/2.5/weather?q=London&APPID=55a967536c6b25bfe368a4c4f5f1f869
-
-//https://api.openweathermap.org/data/2.5/weather?q=London&appid=55a967536c6b25bfe368a4c4f5f1f869
-
-//image - https://openweathermap.org/img/wn/11n@2x.png
-
-import { getData } from './modules/api'
-import { header, ChangeCity, DetailsSection, Footer } from './modules/Doc'
+import { header, DetailsSection } from './modules/Doc'
 import './style/slider.css'
 import './style/head.css'
 import './style/Details.css'
 
-header(); // Import Header in Html.
+header();
 (async function () {
     DetailsSection();
-})();// Add detail section to document.
+})();
